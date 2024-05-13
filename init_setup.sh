c681d487dac3ff566653e1dcf24816208ff13d85
UpendraBWappgo <upendrawappgo@gmail.com>
2024-05-13 17:20:38 +0530

    init

--- init_setup.sh
+++ init_setup.sh
@@ -1,7 +1,8 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating a conda env with python 3.9"
-conda create --prefix ./env python=3.9 -y
+conda create --prefixt ./env python=3.9 -y
 echo [$(date)]: "created conda env"
+source activate ./env
 echo [$(date)]: "activated conda env"
 echo [$(date)]: "installing requirements"
 pip install -r requirements.txt 
